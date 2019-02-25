9dacfd553b0a1bdf5c7dbfeaa685bfb0edebd8ff
Jackson Morais <jheckson99@gmail.com>
2019-02-24 22:05:07 -0300

    Initial Commit

---
+++ js/cat.js
@@ -0,0 +1,149 @@
+function Magica() {
+    particlesJS('particles-js',
+  
+    {
+        "particles": {
+          "number": {
+            "value": 100,
+            "density": {
+              "enable": true,
+              "value_area": 800
+            }
+          },
+          "color": {
+            "value": "#ffffff"
+          },
+          "shape": {
+            "type": "circle",
+            "stroke": {
+              "width": 0,
+              "color": "#000000"
+            },
+            "polygon": {
+              "nb_sides": 5
+            },
+            "image": {
+              "src": "img/github.svg",
+              "width": 100,
+              "height": 100
+            }
+          },
+          "opacity": {
+            "value": 1,
+            "random": false,
+            "anim": {
+              "enable": false,
+              "speed": 1,
+              "opacity_min": 0.1,
+              "sync": false
+            }
+          },
+          "size": {
+            "value": 3,
+            "random": true,
+            "anim": {
+              "enable": true,
+              "speed": 40,
+              "size_min": 0.1,
+              "sync": false
+            }
+          },
+          "line_linked": {
+            "enable": true,
+            "distance": 150,
+            "color": "#ffffff",
+            "opacity": 1,
+            "width": 1
+          },
+          "move": {
+            "enable": true,
+            "speed": 3,
+            "direction": "top-right",
+            "random": false,
+            "straight": false,
+            "out_mode": "out",
+            "bounce": false,
+            "attract": {
+              "enable": false,
+              "rotateX": 600,
+              "rotateY": 1200
+            }
+          }
+        },
+        "interactivity": {
+          "detect_on": "canvas",
+          "events": {
+            "onhover": {
+              "enable": true,
+              "mode": "bubble"
+            },
+            "onclick": {
+              "enable": false,
+              "mode": "push"
+            },
+            "resize": true
+          },
+          "modes": {
+            "grab": {
+              "distance": 400,
+              "line_linked": {
+                "opacity": 1
+              }
+            },
+            "bubble": {
+              "distance": 391.02903705946187,
+              "size": 10,
+              "duration": 0.8409226603429288,
+              "opacity": 0.7904673007223532,
+              "speed": 3
+            },
+            "repulse": {
+              "distance": 200,
+              "duration": 0.4
+            },
+            "push": {
+              "particles_nb": 4
+            },
+            "remove": {
+              "particles_nb": 2
+            }
+          }
+        },
+        "retina_detect": true
+      }
+);
+
+console.log('\n')
+console.log('>>> 😜 Fazendo Magica!')
+console.log('\n')
+
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+$('.click-container').css('opacity', '0')
+setTimeout(() => {
+  $('.click-container').css('display', 'none')
+}, '1700')
+
+setTimeout(() => {
+  $('.cat').css('bottom', '0');
+  setTimeout(() => {
+    $('.glass').css('bottom', '8px');
+    setTimeout(() => {
+      $('#particles-js').css('opacity', '1')
+    }, 500)
+  }, 1000)
+}, 500)
+
+setInterval(function() {  
+  $('.bg').css({'background-color': getRandomColor()})  
+}, 1000);
+
+}
